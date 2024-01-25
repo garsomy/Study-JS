485a9da5d0f0c1b5ce7c804371adc3ef45e95c90
garsomy <92248261+garsomy@users.noreply.github.com>
2024-01-25 14:14:52 +0300

    Add files via upload

---
+++ Study JS.js
@@ -0,0 +1,152 @@
+// 1.1
+// let name = prompt('Как тебя зовут?');
+// console.log(name);
+
+// 1.2
+// let a = 1, b = 1;
+//
+// let c = ++a;
+// let d = b++;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// Ответ 2 2 2 1
+
+// 1.3
+// let a = 2;
+//
+// let x = 1 + (a *= 2);
+//
+// console.log(a) // 4
+// console.log(x) // 5
+
+// 2.1
+// console.log('' + 1 + 0); // 10
+// console.log('' - 1 + 0); // -1
+// console.log(true + false); // 1
+// console.log(6 / '3'); // 2
+// console.log('2' * '3'); // 6
+// console.log(4 + 5 + 'px'); // 9px
+// console.log('$' + 4 + 5); // $45
+// console.log('4' - 2); // 2
+// console.log('  -9  ' + 5); //  -9  5
+// console.log('  -9  ' - 5); // -14
+// console.log(null + 1); // 1
+// console.log(undefined + 1); //  NaN
+// console.log(' /t /n' - 2); // NaN
+
+// 2.2
+// let a = prompt('Первое число?', 1);
+// let b = prompt('Второе число?', 2);
+//
+// alert(Number(a) + Number(b));
+//
+// console.log();
+
+// 3.1
+// let num = [5, 2, 7];
+//
+// alert(num);
+
+// 3.2
+// let a = 10;
+// let b = 2;
+//
+//
+// alert(a + b); // 12
+// alert(a - b); // 8
+// alert(a * b); // 20
+// alert(a / b); // 5
+
+// 3.3
+// let c = 15;
+// let d = 2;
+//
+// let result = c + d
+//
+// alert(result)
+
+// 3.4
+// let a = 10;
+// let b = 2;
+// let c = 5;
+//
+// alert(a + b + c)
+
+// 3.5
+// let a = 17;
+// let b = 10;
+// let c = a - b;
+// let d = 7;
+// let result = c + d
+//
+// alert(result)
+
+// 4.1
+// let str = 'Привет, Мир!'
+//
+// alert('str')
+
+// 4.2
+// let str1 = 'Привет, '
+// let str2 = 'Мир!'
+//
+// alert(str1 + str2)
+
+// 4.3
+// let name = 'Даниил'
+//
+// alert('Привет, ' + name)
+
+// 4.4
+// let age = 16
+//
+// alert('Мне ' + age + ' лет!')
+
+// 5.1
+// let name = prompt('Как тебя зовут?')
+//
+// alert('Ваше имя ' + name)
+
+// 5.2
+// let num = prompt('Введите число')
+//
+// alert('Ваше число ' + num ** 2)
+
+// 6.1
+// let hours = (1 * 60 * 60);
+// let day = (1 * 24 * 60 * 60);
+// let month = (1 * 31 * 24 * 60 * 60 * 60);
+//
+// alert('В часе столько секунд: ' + hours);
+// alert('В дне столько секунд: ' + day);
+// alert('В месяце столько секунд: ' + month);
+
+// 6.2
+// let hours = prompt('Какой сейчас час?');
+// let minute = prompt('Какая сейчас минута?');
+// let seconds = prompt('Какая сейчас секунда?');
+//
+// alert(hours + ':' + minute + ':' + seconds);
+
+// 6.3
+// let num = 5;
+//
+// alert(num ** 2)
+
+// 7.1
+// var num = 47;
+// num += 7;
+// num -= 18;
+// num *= 10;
+// num /= 15;
+// alert(num);
+
+// 7.2
+// var num = 10;
+// num = (++num);
+// num = (++num);
+// num = (--num);
+// alert(num);
